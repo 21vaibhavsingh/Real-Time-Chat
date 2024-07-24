c9900e8831b9d2611653c8d40d9946bb7f6fa1b1
vaibhav pratap singh <21vaibhavps@gmail.com>
2024-07-25 01:19:58 +0530

    frontend file

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
- // const express = require('express')  // method -1
-import express from "express";   //method -2
-import dotenv from "dotenv";
-import connectDB from "./config/database.js";
-import userRoute from "./routes/userRoute.js";
-import msgRoute from "./routes/msgRoute.js"
-import cookieParser from "cookie-parser";
-import cors from "cors"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
 
-dotenv.config({});
+let persistor = persistStore(store);
 
-const app = express();
+export const BASE_URL="http://localhost:8080"
 
-const PORT =process.env.PORT || 8080;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
 
-//middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(cookieParser());
-const corsOption={
-    origin:'http://localhost:3000',
-    credentials:true
-};
-app.use(cors(corsOption)); 
-//routes
-
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/msg",msgRoute);
-
-app.listen(PORT, ()=>{
-
-    connectDB();
-    console.log(`server listen at port ${PORT}`);
-});
