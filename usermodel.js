90eac6215e2d6e1950488234c0d9b463ec4d4b14
vaibhav pratap singh <21vaibhavps@gmail.com>
2024-07-25 01:03:59 +0530

    added successfully

---
+++ usermodel.js
@@ -0,0 +1,28 @@
+import { request } from "express";
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema({
+    fullName:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    profilePhoto:{
+        type:String,
+        default:" "
+    },
+    gender:{
+        type:String,
+        enum:["male","female"],
+        required:true
+    }
+} , {timestamps:true});
+export const user = mongoose.model("User",userModel);
